bf8287dd2cad348c1e06e6d27139366e2036a96e
M3mori3 <nylmorilla@gmail.com>
2024-09-24 21:40:41 +0800

    initial commit

---
+++ CS2C_LynmuelLMorilla_C1-A1.js
@@ -0,0 +1,59 @@
+// Variable/Constants Declaration and String Concatenation Methods
+
+//Persona 1
+let fName1 = "Mark Vincent";
+let mName1 = "Gaoat";
+let lName1 = "Ricarde";
+let bDate1 = "February 8, 2005 ";
+let bPlace1 = "Brgy. Butol, Santiago, Philippines, 2707 ";
+let addr1 = bPlace1;
+let courseYr1 = "Bachelor of Science in Computer Science";
+let djob1 = "Motor Tuner"; 
+
+let fullName1 = fName1 +" "+ mName1+ " "+ lName1;
+let upFullName1 = fullName1.toUpperCase();
+
+
+let fsentence1 = upFullName1 + " was born in " + bDate1 + " at " + bPlace1.toUpperCase() + "and is currently living there. ";
+let sSentence1 = fullName1 + " is taking up " + courseYr1 + " and dreams to be a " + djob1 + " after graduation. ";
+
+console.log(fsentence1+sSentence1);
+console.log(" ");
+
+// Persona 2
+let fName2 = "Mark Jasson";
+let mName2 = "Egos";
+let lName2 = "Dapon";
+let bDate2 = "April 18, 2005";
+let bPlace2 = "Brgy. Tablac, Candon City, Philippines, 2710 " ;
+let addr2 = bPlace2 ;
+let courseYr2 = "Bachelor of Science in Computer Science";
+let djob2 = "Game Developer";
+
+let fullName2 = fName2 +" "+ mName2+ " "+ lName2;
+let upFullName2 = fullName2.toUpperCase();
+
+let fsentence2 = "His classmate " + upFullName2 + ", was born in " + bDate2 + " at " + bPlace2.toUpperCase() + "and has been living there ever since. ";
+let sSentence2 = fullName2 + " is taking up " + courseYr2 + " and wants to become a " + djob2 + " after graduation. ";
+
+console.log(fsentence2 + sSentence2);
+console.log(" ");
+
+// Persona 3
+let fName3 = "Lynmuel";
+let mName3 = "Lucero";
+let lName3 = "Morilla";
+let bDate3 = "March 30, 2005";
+let bPlace3 = "Brgy. Nagkaisang Nayon, Quezon City, Philippines, 1123 " ;
+let addr3 = "Brgy. Villa Garcia, Sta. Cruz, Philippines, 2713";
+let courseYr3 = "Bachelor of Science in Computer Science";
+let djob3 = "Game Developer";
+
+let fullName3 = fName3 +" "+ mName3+ " "+ lName3;
+let upFullName3 = fullName3.toUpperCase();
+
+let fsentence3 = "Their other classmate " + upFullName3 + ", was born in " + bDate3 + " at " + bPlace3.toUpperCase() + " and is now living in " + addr3.toUpperCase() + ". ";
+let sSentence3 = fullName3 + " is taking up " + courseYr3 + " and also wants to become a " + djob3 + " after graduation.";
+
+console.log(fsentence3 + sSentence3);
+console.log(" ");
---
+++ CS2C_LynmuelLMorilla_C1-A2.js
@@ -0,0 +1,29 @@
+// Mathematical and Comparison Operations, and Type Conversions
+
+let a = 25;
+let b = 20;
+let c = 12;
+let d = "15";
+const e = 15;
+
+let sumOfAll =(25 + 20 + 12 + "15" + 15);
+console.log(sumOfAll);
+
+console.log("15" > 15);
+console.log("15" < 15);
+console.log("15" >= 15);
+console.log("15" <= 15);
+console.log("15" === 15);
+
+let subVar = (a - b - c - d - e);
+let mulVar = ((a * c )/ e);
+let exOfE = (e ** c);
+
+console.log(subVar);
+console.log(mulVar);
+console.log(exOfE);
+
+let reExOfE = exOfE;
+reExOfE = e;
+
+console.log(reExOfE);
